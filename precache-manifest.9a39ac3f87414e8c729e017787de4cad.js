4c704ddfde0e8760c488c62f62e32b03bad88769
Marcin Hercog <mkp.hercog@gmail.com>
2020-04-08 18:04:15 +0200

    Updates

---
+++ precache-manifest.9a39ac3f87414e8c729e017787de4cad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5531fcdcc6fa2210eb279288df9dca5",
+    "url": "/musicfromspotify/index.html"
+  },
+  {
+    "revision": "85d6795a727e8d2a022b",
+    "url": "/musicfromspotify/static/css/main.4bc045ce.chunk.css"
+  },
+  {
+    "revision": "6fff8b7b5c0272ebfb04",
+    "url": "/musicfromspotify/static/js/2.5b1e3a36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/musicfromspotify/static/js/2.5b1e3a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85d6795a727e8d2a022b",
+    "url": "/musicfromspotify/static/js/main.ebddfc70.chunk.js"
+  },
+  {
+    "revision": "3edeb2cea739ed61c65d",
+    "url": "/musicfromspotify/static/js/runtime-main.b8f53c11.js"
+  },
+  {
+    "revision": "1df5799c8dbd077135d5fad105b83fa8",
+    "url": "/musicfromspotify/static/media/loginHeadphones.1df5799c.jpg"
+  }
+]);
