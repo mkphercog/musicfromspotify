4c8c3b82292a07f927ee04ed79294faff399dad6
Marcin Hercog <mkp.hercog@gmail.com>
2020-05-24 12:13:26 +0200

    add anchor with author link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,6 +3,16 @@ import "./Footer.scss";
 
 export const Footer: React.FC = () => (
   <footer className="footer">
-    <p className="footer__author">Projekt i realizacja Marcin Hercog</p>
+    <p className="footer__author">
+      Projekt i realizacja{" "}
+      <a
+        className="footer__author--link"
+        href="https://mkphercog.github.io/mywebsite"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Marcin Hercog
+      </a>
+    </p>
   </footer>
 );
